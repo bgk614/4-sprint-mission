ef6591442449454eae0c604d716ec39d13a7a521
Bogyeong Kim <bgk614@gmail.com>
2025-07-25 00:00:54 +0900

    refactor: 상품 배열 생성 함수 분리

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import {
   createProduct,
   patchProduct,
   deleteProduct,
-  Product,
-  ElectronicProduct,
-} from './products/index.js';
+  makeProductArray,
+} from "./products/index.js";
 
 import {
   getArticleList,
@@ -14,40 +13,7 @@ import {
   createArticle,
   patchArticle,
   deleteArticle,
-} from './articles/index.js';
-
-const productListInstance = async (page, pageSize) => {
-  console.log('=== 작업 1 : 상품 리스트 각각 products 배열에 저장 ===');
-
-  const res = await getProductList({ page, pageSize });
-  const products = [];
-
-  res.list.forEach((item) => {
-    let instance;
-    const tags = Array.isArray(item.tags) ? item.tags : [];
-    if (tags.includes('전자제품')) {
-      instance = new ElectronicProduct(
-        item.name,
-        item.description,
-        item.price,
-        item.tags,
-        item.images,
-        item.manufacturer
-      );
-    } else {
-      instance = new Product(
-        item.name,
-        item.description,
-        item.price,
-        item.tags,
-        item.images
-      );
-    }
-    products.push(instance);
-  });
-
-  console.log(products);
-};
+} from "./articles/index.js";
 
 const functionsTest = async (
   page,
@@ -59,65 +25,64 @@ const functionsTest = async (
   const test = (name) => {
     console.log(`\n[get${name}List]`);
     console.log(àwait`get${name}List`({ page, pageSize, keyword }));
+  };
+  console.log("=== 작업 2 : 함수 테스트 ===");
 
-  }
-  console.log('=== 작업 2 : 함수 테스트 ===');
-
-  console.log('\n[getProductList]');
+  console.log("\n[getProductList]");
   console.log(await getProductList({ page, pageSize, keyword }));
 
-  console.log('\n[createProduct]');
+  console.log("\n[createProduct]");
   const createdProduct = await createProduct(productBody);
   console.log(createdProduct);
   const productId = createdProduct.id;
 
-  console.log('\n[getProduct]');
+  console.log("\n[getProduct]");
   console.log(await getProduct(productId));
 
-  console.log('\n[patchProduct]');
+  console.log("\n[patchProduct]");
   console.log(await patchProduct(productId, productBody));
 
-  console.log('\n[deleteProduct');
+  console.log("\n[deleteProduct");
   console.log(await deleteProduct(productId));
 
-  console.log('\n[getArticleList]');
+  console.log("\n[getArticleList]");
   console.log(await getArticleList({ page, pageSize, keyword }));
 
-  console.log('\n[createArticle]');
+  console.log("\n[createArticle]");
   const createdArticle = await createArticle(articleBody);
   console.log(createdArticle);
   const articleId = createdArticle.id;
 
-  console.log('\n[getArticle]');
+  console.log("\n[getArticle]");
   console.log(await getArticle(articleId));
 
-  console.log('\n[patchArticle]');
+  console.log("\n[patchArticle]");
   console.log(await patchArticle(articleId, articleBody));
 
-  console.log('\n[deleteArticle]');
+  console.log("\n[deleteArticle]");
   console.log(await deleteArticle(articleId));
 };
 
 const main = async () => {
   const page = 1;
   const pageSize = 1;
-  const keyword = '';
+  const keyword = "";
 
   const articleBody = {
-    image: 'https://example.com/...',
-    content: '게시글 내용입니다.',
-    title: '제목',
+    image: "https://example.com/...",
+    content: "게시글 내용입니다.",
+    title: "제목",
   };
 
   const productBody = {
-    images: ['https://example.com/...'],
-    tags: ['전자제품'],
+    images: ["https://example.com/..."],
+    tags: ["전자제품"],
     price: 0,
-    description: 'string',
-    name: '상품 이름',
+    description: "string",
+    name: "상품 이름",
   };
 
-  await productListInstance(page, pageSize); // 작업 1
+  await makeProductArray(page, pageSize); // 작업 1
   await functionsTest(page, pageSize, keyword, articleBody, productBody); // 작업 2
 };
 
--- src/products/index.js
+++ src/products/index.js
@@ -1,3 +1,4 @@
 export { default as Product } from "./Product.js";
 export { default as ElectronicProduct } from "./ElectronicProduct.js";
 export * from "./ProductService.js";
+export * from "./ProductUtils.js";
