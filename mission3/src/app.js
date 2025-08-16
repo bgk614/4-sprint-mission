745fd06c0d75ca134fd31501155a15530fb464b5
Bogyeong Kim <bgk614@gmail.com>
2025-08-16 23:12:16 +0900

    feat: swagger api docs

--- mission3/src/app.js
+++ mission3/src/app.js
@@ -1,24 +1,27 @@
-import swaggerUi from 'swagger-ui-express';
 import express from 'express';
 import dotenv from 'dotenv';
-import { openapiSpecification } from '../swagger.js';
+import swaggerUi from 'swagger-ui-express';
+
 import { errorHandler } from './middlewares/errorHandler.js';
 import { productRouter } from './product/product.routes.js';
 import { articleRouter } from './article/article.routes.js';
 
+import swaggerDocument from './docs/openapi-resolved.json' with { type: 'json' };
+
 dotenv.config();
 
-const port = process.env.PORT;
-export const app = express();
+const app = express();
+const PORT = process.env.PORT || 1000;
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/products', productRouter);
 app.use('/articles', articleRouter);
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`${port} 포트에서 서버 시작`);
+app.listen(PORT, () => {
+  console.log(`${PORT} 포트에서 서버 시작`);
 });
