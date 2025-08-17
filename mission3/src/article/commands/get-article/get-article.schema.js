92104c1bd43fda3014bff5c5843e4d53d2a6fbd5
Bogyeong Kim <bgk614@gmail.com>
2025-08-17 19:04:56 +0900

    feat: update Zod schema to coerce path params into numbers

--- mission3/src/article/commands/get-article/get-article.schema.js
+++ mission3/src/article/commands/get-article/get-article.schema.js
@@ -1,9 +1,7 @@
 import { z } from 'zod';
 
 export const getArticleSchema = z.object({
-  id: z
-    .string()
-    .regex(/^\d+$/, { message: 'id는 양의 정수 문자열이어야 합니다.' })
-    .transform((v) => Number(v))
-    .refine((n) => n > 0, { message: 'id는 1 이상의 정수여야 합니다.' })
+  id: z.coerce.number().int().positive({
+    message: 'id는 1 이상의 정수여야 합니다.'
+  })
 });
