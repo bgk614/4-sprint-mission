85d27822d24502772a4a88042e9c1c923495a91c
Bogyeong Kim <bgk614@gmail.com>
2025-09-14 23:14:53 +0900

    feat: auth

--- mission4/src/components/auth/tokens/tokens.controller.js
+++ mission4/src/components/auth/tokens/tokens.controller.js
@@ -1,11 +1,14 @@
-import { refreshTokenService } from './tokens.service.js';
+import { AppError } from '../../../utils/app-error.js';
+import { refreshAccessTokenService } from './tokens.service.js';
 
-export const refreshTokenController = async (req, res, next) => {
+export const refreshAccessTokenController = async (req, res, next) => {
   try {
     const { refreshToken } = req.body; // 또는 쿠키에서 가져올 수도 있음
-    const newToken = await refreshTokenService(refreshToken);
+    if (!refreshToken) throw new AppError(400, 'BAD_REQUEST', 'Refresh Token 필요');
 
-    res.status(200).json({ accessToken: newToken });
+    const newAccessToken = await refreshAccessTokenService(refreshToken);
+
+    res.status(200).json({ accessToken: newAccessToken });
   } catch (err) {
     next(err);
   }
