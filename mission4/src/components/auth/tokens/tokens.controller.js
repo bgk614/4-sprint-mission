b528dffc47d91b5f9833eec13918390942880222
Bogyeong Kim <bgk614@gmail.com>
2025-09-14 23:32:13 +0900

    chore: update tokens

--- mission4/src/components/auth/tokens/tokens.controller.js
+++ mission4/src/components/auth/tokens/tokens.controller.js
@@ -1,14 +1,14 @@
 import { AppError } from '../../../utils/app-error.js';
-import { refreshAccessTokenService } from './tokens.service.js';
+import { refreshTokenService } from './tokens.service.js';
 
-export const refreshAccessTokenController = async (req, res, next) => {
+export const refreshTokenController = async (req, res, next) => {
   try {
     const { refreshToken } = req.body; // 또는 쿠키에서 가져올 수도 있음
     if (!refreshToken) throw new AppError(400, 'BAD_REQUEST', 'Refresh Token 필요');
 
-    const newAccessToken = await refreshAccessTokenService(refreshToken);
+    const newToken = await refreshTokenService(refreshToken);
 
-    res.status(200).json({ accessToken: newAccessToken });
+    res.status(200).json({ accessToken: newToken });
   } catch (err) {
     next(err);
   }
