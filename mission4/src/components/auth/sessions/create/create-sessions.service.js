d61ea9e3ee3057e91ec5f2b77b95bf28b91e5fd1
Bogyeong Kim <bgk614@gmail.com>
2025-09-14 23:14:53 +0900

    feat: auth

--- mission4/src/components/auth/sessions/create/create-sessions.service.js
+++ mission4/src/components/auth/sessions/create/create-sessions.service.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 import prisma from '../../../../config/prisma.js';
 import { generateTokens } from '../../../../config/token.js';
--- mission4/src/components/auth/tokens/tokens.router.js
+++ mission4/src/components/auth/tokens/tokens.router.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import { refreshAccessTokenController } from './tokens.controller.js';
+
 export const tokenRouter = express.Router();
 
-// tokenRouter.route('/refresh').post(); // 토큰 재발급
+tokenRouter.route('/refresh').post(refreshAccessTokenController); // 토큰 재발급
