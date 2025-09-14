e84d7efb20b179d4ad37159411f4b0c4b843eb24
Bogyeong Kim <bgk614@gmail.com>
2025-09-15 00:00:53 +0900

    chore: update constants.js

--- mission4/src/config/constants.js
+++ mission4/src/config/constants.js
@@ -1,18 +1,8 @@
-const PORT = process.env.PORT || 3000;
-const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
+export const PORT = process.env.PORT || 3000;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
 
-export { PORT, SESSION_SECRET };
+export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET || 'your_jwt_access_token_secret';
+export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET || 'your_jwt_refresh_token_secret';
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET || 'your_jwt_access_token_secret';
-const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET || 'your_jwt_refresh_token_secret';
-const ACCESS_TOKEN_COOKIE_NAME = 'access-token';
-const REFRESH_TOKEN_COOKIE_NAME = 'refresh-token';
-
-export {
-  ACCESS_TOKEN_COOKIE_NAME,
-  JWT_ACCESS_TOKEN_SECRET,
-  JWT_REFRESH_TOKEN_SECRET,
-  NODE_ENV,
-  REFRESH_TOKEN_COOKIE_NAME,
-};
+export const ACCESS_TOKEN_COOKIE_NAME = 'access-token';
+export const REFRESH_TOKEN_COOKIE_NAME = 'refresh-token';
