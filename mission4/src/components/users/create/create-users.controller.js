188fd6581e84e871f768b9e6a2f960a0330bc1ec
Bogyeong Kim <bgk614@gmail.com>
2025-09-14 16:52:01 +0900

    feat: create user

--- mission4/src/components/users/create/create-users.controller.js
+++ mission4/src/components/users/create/create-users.controller.js
@@ -2,8 +2,8 @@ import { createUserService } from './create-users.service.js';
 
 export async function createUserController(req, res, next) {
   try {
-    const { email, nickname, password, image } = req.body;
-    const user = await createUserService(email, nickname, password, image);
+    const { email, nickname, password } = req.body;
+    const user = await createUserService(email, nickname, password);
     res.status(201).json(user);
   } catch (err) {
     next(err);
--- mission4/src/components/users/create/create-users.service.js
+++ mission4/src/components/users/create/create-users.service.js
@@ -1,3 +1,29 @@
+import bcrypt from 'bcryptjs';
+
+import prisma from '../../../config/prisma.js';
+
+export async function createUserService(email, nickname, password) {
+  // 비밀번호 해싱
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // 유저 생성
+  const user = await prisma.user.create({
+    data: {
+      email,
+      nickname,
+      password: hashedPassword,
+    },
+    select: {
+      id: true,
+      email: true,
+      nickname: true,
+      createdAt: true,
+    },
+  });
+
+  return user;
+}
+
 import bcrypt from 'bcrypt';
 
 import prisma from '../../../config/prisma.js';
