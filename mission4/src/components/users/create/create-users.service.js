a9380a8337d01819fb1f117c26605b62b158c802
Bogyeong Kim <bgk614@gmail.com>
2025-09-22 09:48:18 +0900

    fix: resolve conflicts in create-users.service.js

--- mission4/src/components/users/create/create-users.service.js
+++ mission4/src/components/users/create/create-users.service.js
@@ -25,31 +25,3 @@ export async function createUserService(email, nickname, password, image) {
 
   return user;
 }
-
-import bcrypt from 'bcrypt';
-
-import prisma from '../../../config/prisma.js';
-
-export async function createUserService(email, nickname, password, image) {
-  // 비밀번호 해싱
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // 유저 생성
-  const user = await prisma.user.create({
-    data: {
-      email,
-      nickname,
-      password: hashedPassword,
-      image,
-    },
-    select: {
-      id: true,
-      email: true,
-      nickname: true,
-      createdAt: true,
-      image: true,
-    },
-  });
-
-  return user;
-}
